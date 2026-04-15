78a90bfbd4275953dfc5c3adf6bf2c6ca6bac8cc
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-15 21:11:54 +0200

    fixed: -quando si generavano nuove mail non eliminava la lista vecchia ma aggiungeva solo -rimosso l'id sui list item e aggiunta la classe list-item-custom per gestire le animazione di hover

--- script/script.js
+++ script/script.js
@@ -9,6 +9,7 @@ const emailCount = document.querySelector('#email-count');
 
 
 function emailsCall(nEmails) {
+  const richieste = [];
     for (let i = 0; i < nEmails; i++) {
       const promessa = fetch(emailAPI)
         .then(risposta => risposta.json());
@@ -26,14 +27,14 @@ function emailsCall(nEmails) {
 function listGenerator(array) {
     let list = '';
     array.forEach(element => {
-        list += `<li class="list-group-item border border-success text-success col-6" id="list-item">${element}</li>`;
+        list += `<li class="list-group-item border border-success text-success col-6 list-item-custom">${element}</li>`;
     });
     emailListSelector.innerHTML = list;
 }
 
 
-emailsCall(15);
+emailsCall(10);
 emailForm.addEventListener('submit',(event) => {
     event.preventDefault();
-    emailsCall(emailCount.value);
+    emailsCall(parseInt(emailCount.value));
 })

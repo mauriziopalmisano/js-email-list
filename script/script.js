81c304b11c5ebb6a305d793e291e8bd2018ca3ab
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-15 19:35:10 +0200

    aggiunta un pó di grafica implementazione dellaggiunta di mail con un bottone ma non si cancellano quelle generate precedentemente

--- script/script.js
+++ script/script.js
@@ -4,22 +4,20 @@ const emailAPI = 'https://flynn.boolean.careers/exercises/api/random/mail';
 const richieste = [];
 let elementList = undefined;
 const emailListSelector = document.querySelector('#email-list');
+const emailForm = document.querySelector('#email-form');
+const emailCount = document.querySelector('#email-count');
 
 
 function emailsCall(nEmails) {
-    
     for (let i = 0; i < nEmails; i++) {
       const promessa = fetch(emailAPI)
         .then(risposta => risposta.json());
-        
       richieste.push(promessa);
     }
     
     Promise.all(richieste)
       .then(risultati => {
-        console.log(risultati);
         const emails = risultati.map(risultato => risultato.response);
-        console.log("Tutte le mail:", emails);
         listGenerator(emails);
       })
 }
@@ -28,10 +26,14 @@ function emailsCall(nEmails) {
 function listGenerator(array) {
     let list = '';
     array.forEach(element => {
-        list += `<li>${element}</li>`;
+        list += `<li class="list-group-item border border-success text-success col-6" id="list-item">${element}</li>`;
     });
     emailListSelector.innerHTML = list;
 }
 
 
 emailsCall(15);
+emailForm.addEventListener('submit',(event) => {
+    event.preventDefault();
+    emailsCall(emailCount.value);
+})

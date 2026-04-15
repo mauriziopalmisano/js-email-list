97586d62af87d1fe42414a3d07e28f9627b58cfc
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-04-15 17:35:41 +0200

    aggiunta della logica js per gestire le diverse fetch

--- script/script.js
+++ script/script.js
@@ -1,3 +1,37 @@
 'use strict';
 
+const emailAPI = 'https://flynn.boolean.careers/exercises/api/random/mail';
+const richieste = [];
+let elementList = undefined;
+const emailListSelector = document.querySelector('#email-list');
 
+
+function emailsCall(nEmails) {
+    
+    for (let i = 0; i < nEmails; i++) {
+      const promessa = fetch(emailAPI)
+        .then(risposta => risposta.json());
+        
+      richieste.push(promessa);
+    }
+    
+    Promise.all(richieste)
+      .then(risultati => {
+        console.log(risultati);
+        const emails = risultati.map(risultato => risultato.response);
+        console.log("Tutte le mail:", emails);
+        listGenerator(emails);
+      })
+}
+
+
+function listGenerator(array) {
+    let list = '';
+    array.forEach(element => {
+        list += `<li>${element}</li>`;
+    });
+    emailListSelector.innerHTML = list;
+}
+
+
+emailsCall(15);
